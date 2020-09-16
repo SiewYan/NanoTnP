791ba116945850eecdc0a3b3c422680715296b3f
arun <arun.kumar@cern.ch>
2020-09-16 10:41:42 +0200

    up

---
+++ analysis/scripts/run2018_v1.sh
@@ -0,0 +1,9 @@
+python tnpEGM_fitter.py settings/nanov7/settings_nanov7_18.py --flag passingMVA94Xwp90isoHWWiso0p06 --checkBins
+python tnpEGM_fitter.py settings/nanov7/settings_nanov7_18.py --flag passingMVA94Xwp90isoHWWiso0p06 --createBins
+python tnpEGM_fitter.py settings/nanov7/settings_nanov7_18.py --flag passingMVA94Xwp90isoHWWiso0p06 --createHist
+python tnpEGM_fitter.py settings/nanov7/settings_nanov7_18.py --flag passingMVA94Xwp90isoHWWiso0p06 --doFit
+python tnpEGM_fitter.py settings/nanov7/settings_nanov7_18.py --flag passingMVA94Xwp90isoHWWiso0p06 --doFit --mcSig --altSig
+python tnpEGM_fitter.py settings/nanov7/settings_nanov7_18.py --flag passingMVA94Xwp90isoHWWiso0p06 --doFit --altSig
+python tnpEGM_fitter.py settings/nanov7/settings_nanov7_18.py --flag passingMVA94Xwp90isoHWWiso0p06 --doFit --altBkg
+python tnpEGM_fitter.py settings/nanov7/settings_nanov7_18.py --flag passingMVA94Xwp90isoHWWiso0p06 --sumUp
+
